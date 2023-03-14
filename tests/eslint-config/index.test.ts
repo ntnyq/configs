00156957080e260fe34e1a4a8c551eed6f30177d
ntnyq <ntnyq13@gmail.com>
2023-03-14 14:48:32 +0800

    test: fix test

--- tests/eslint-config/index.test.ts
+++ tests/eslint-config/index.test.ts
@@ -9,29 +9,13 @@ describe('invalid', () => {
     const eslint = new ESLint({ overrideConfigFile })
     const result = await eslint.lintText('export const foo = `bar`')
 
-    expect(result[0].errorCount).toBe(2)
+    expect(result[0].errorCount).toBe(0)
     expect(result[0].messages).toMatchInlineSnapshot(`
       [
         {
-          "column": 20,
+          "column": 25,
           "endColumn": 25,
           "endLine": 1,
-          "fix": {
-            "range": [
-              19,
-              24,
-            ],
-            "text": "'bar'",
-          },
-          "line": 1,
-          "message": "Strings must use singlequote.",
-          "messageId": "wrongQuotes",
-          "nodeType": "TemplateLiteral",
-          "ruleId": "@typescript-eslint/quotes",
-          "severity": 2,
-        },
-        {
-          "column": 25,
           "fix": {
             "range": [
               24,
@@ -41,11 +25,11 @@ describe('invalid', () => {
       ",
           },
           "line": 1,
-          "message": "Newline required at end of file but not found.",
-          "messageId": "missing",
-          "nodeType": "Program",
-          "ruleId": "eol-last",
-          "severity": 2,
+          "message": "Insert \`⏎\`",
+          "messageId": "insert",
+          "nodeType": null,
+          "ruleId": "prettier/prettier",
+          "severity": 1,
         },
       ]
     `)
