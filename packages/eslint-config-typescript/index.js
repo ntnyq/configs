90320e4d1553083da7496c19367d015d14a91d65
ntnyq <ntnyq13@gmail.com>
2023-07-11 23:42:15 +0800

    feat: support typescript-eslint v6

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -9,6 +9,7 @@ module.exports = {
     '@ntnyq/eslint-config-basic',
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
   ],
 
   settings: {
